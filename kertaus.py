398c43163b8b9b907528fec3c24e15f6bc30f967
Furqan Noor <82640979+FurqanNoor@users.noreply.github.com>
2021-05-28 10:00:50 +0300

    Add files via upload
    New fixed files

--- kertaus.py
+++ kertaus.py
@@ -1,51 +1,40 @@
 import random
 
-# Tässä se lukee sanat.txt tiedoston
-
 with open('sanat.txt', encoding='utf-8') as f:
     data = f.read().splitlines()
 print(data)
 
-# Tässä on kaikki merkkijonot
-
 string = random.choices(data, k=2)
 separator = ","
 value = True
+def lowercase_list(item):
+            return item.lower()
 while value:
     try:
         integer = int(input('Anna kokonaisluku:'))
         value = False
-    except:
+    except ValueError:
         print("Kokonaisluvussa ei ole desimaalia")
+        continue
     else:
         for x in range(integer):
-            print(separator.join(string))
+            if integer > 0:
+                print(separator.join(string))
 
-# Tässä se tulostaa ensimmäisen luvun jos se on jaollinen kolmella
 
-    num1 = integer
-    if  num1 % 3 == 0:
+    if((integer%3==0) and (integer%5==0)):
+        print (string[0],string[1])
+    elif  integer % 3 == 0:
         print(string[0])
+    elif integer % 5 == 0:
+        print(string[1])
 
-# Tässä se tulostaa viimeisen luvun jos se on jaollinen viidellä
-
-    elif num1 % 5 == 0:
-        print(string[-1])
-
-# Tässä se tulostaa viimeinen ja ensimmäinen sana jos luku on jaollinen kolemella ja viidellä
-
-    elif num1%3==0 & num1%5==0:
-        print (string[0 & -1])
-
-# Tässä se tulostaa kierroksen alkaen luvusta 1 ja päättyen lukuun jonka käyttäjä on antanut.
 
     round = int(input("Kuinka monta kierrosta haluat?"))
     for i in range(round):
         print(i+1)
 
-#Tässä se tulostaa satunnaisesti 25% tulostuskierooista koko sana pienillä kirjaimilla.
-
-    if random.randint(0,100) < 24:
-        print(string)
+    if random.randint(1,100) <= 25:
+        print(list(map(lowercase_list,string)))
 
     value = True
